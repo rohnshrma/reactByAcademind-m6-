d41767c78a0704aac4783422588f85f69a8b4c10
rohnshrma <rohn11shrma@gmail.com>
2022-10-31 16:50:20 +0530

    setting css classes dynamically

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,7 +5,10 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
-  const [isValid, setIsValid] = useState(true);
+
+// to manage events , if the input is blank
+  const [isValid , setIsValid] = useState(true)
+
 
   const goalInputChangeHandler = event => {
     if(event.target.value.trim().length > 0){
@@ -16,18 +19,20 @@ const CourseInput = props => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    // prevent to add blank data . stops the futher execution
     if(enteredValue.trim().length === 0){
       setIsValid(false)
       return;
     }
+    // will not be executed if the length == 0
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color : !isValid ? "red" : "black"}} >Make Some Goal</label>
-        <input type="text" style={{borderColor : !isValid  ?  "red" : "#ccc" }} onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label >{!isValid ? "Nothing to Add!" : "Make Some Goal"}</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
